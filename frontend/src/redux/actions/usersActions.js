d2bc4a0da420efffc104879c121bcfb4f43798c7
Rishat <rishatfattahov22@gmail.com>
2025-12-23 09:24:33 +0300

    create

--- frontend/src/redux/actions/usersActions.js
+++ frontend/src/redux/actions/usersActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/users";
+const API = `${process.env.REACT_APP_API_URL}/users`;
 
 export const fetchFeed = () => async (dispatch, getState) => {
   dispatch({ type: "USERS_LOADING" });
