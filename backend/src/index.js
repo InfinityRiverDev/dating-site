46cd795daace7a876a700fe71a3b0e2d921fc2eb
Rishat <rishatfattahov22@gmail.com>
2025-12-23 09:50:46 +0300

    fixx

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,11 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: "*", // или твой фронтенд https://your-frontend.com
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors);
 app.use(express.json({ limit: "5mb" })); // для base64 фото
 
 app.use("/auth", authRoutes);
